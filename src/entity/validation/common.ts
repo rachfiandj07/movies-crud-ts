a4427528adbac347a00b24a8762e68d6989883a2
Muhammad Naufal Rachfian dJ <naufalrdj@gmail.com>
2021-09-30 16:58:38 +0700

    add common validation on movies & author

--- src/entity/validation/common.ts
+++ src/entity/validation/common.ts
@@ -1,34 +1,35 @@
 import * as Joi from 'joi';
 import { IObject } from 'rey-common';
 
-export const COMMON_SCHEME = {
-    PAGINATION: Joi.object({
-        page: Joi.number().integer().positive().default(1).optional(),
-        per_page: Joi.number().integer().positive().default(10).optional(),
-        sort: Joi.string().optional().default('-created_at')
-    })
-};
-
 export const SCHEME: IObject<Joi.ObjectSchema> = {
-    LOGIN: Joi.object({
+    CREATE_MOVIES: Joi.object({
         body: Joi.object({
-            username: Joi.string().required(),
-            password: Joi.string().required()
+            author_id: Joi.number().required(),
+            movies_name: Joi.string().min(4).required(),
+            content: Joi.string().min(4).max(100).required()
         }).required()
     }),
-    CREATE_POST: Joi.object({
+    UPDATE_MOVIES: Joi.object({
         body: Joi.object({
-            title: Joi.string().min(4).max(50).required(),
-            content: Joi.string().min(10).required()
-        }).required()
-    }),
-    UPDATE_POST: Joi.object({
-        body: Joi.object({
-            title: Joi.string().min(4).max(50).optional(),
-            content: Joi.string().min(10).optional()
+            author_id: Joi.number().optional(),
+            movies_name: Joi.string().min(4).optional(),
+            content: Joi.string().min(4).max(100).optional()
         }).required(),
         params: Joi.object({
-            id: Joi.string().required()
+            movies_id: Joi.number().required()
+        }).required()
+    }),
+    CREATE_AUTHOR: Joi.object({
+        body: Joi.object({
+            author_name: Joi.string().min(4).max(50).required(),
+        }).required()
+    }),
+    UPDATE_AUTHOR: Joi.object({
+        body: Joi.object({
+            author_name: Joi.string().min(4).max(50).optional(),
+        }).required(),
+        params: Joi.object({
+            author_id: Joi.number().required()
         }).required()
     })
 };
