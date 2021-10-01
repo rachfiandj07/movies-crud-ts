6de16112c25eda914812db25dc0894966f90eace
Muhammad Naufal Rachfian dJ <naufalrdj@gmail.com>
2021-10-01 13:02:21 +0700

    adding id to models

--- src/entity/models/author.ts
+++ src/entity/models/author.ts
@@ -2,6 +2,7 @@ import { BaseProps } from 'rey-common';
 import { MoviesProperties } from './movies';
 
 export interface UserProperties extends BaseProps {
+    author_id: number;
     author_name: string;
 
     movies?: MoviesProperties[];
--- src/entity/models/movies.ts
+++ src/entity/models/movies.ts
@@ -1,6 +1,7 @@
 import { BaseProps } from 'rey-common';
 
 export interface MoviesProperties extends BaseProps {
+    movies_id: number;
     author_id: number;
     movies_name: string;
     content: string;
